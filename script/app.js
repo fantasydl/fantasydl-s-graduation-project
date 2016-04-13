e33ee3fad17dd0f4c2ed241764cca7928e67ac9f
fantasydl <406215730@qq.com>
2016-04-13 14:36:12 +0800

    test angularjs
    test angularjs

--- script/app.js
+++ script/app.js
@@ -1,2 +1,19 @@
 // 创建模型
 var indexApp = angular.module('indexApp',[]);
+// var indexApp = angular.module('indexApp',['ui-router']);
+
+// 创建嵌套路由
+// indexApp.config(function($stateProvider,$urlRouterProvider){
+
+// 	$urlRouterProvider.when("","/index");
+
+// 	$stateProvider
+// 		.state('index',{
+// 			url : "/index",
+// 			templateUrl : "index.html"
+// 		})
+// 		.state('index.list',{
+// 			url : "/list",
+// 			templateUrl : "list.html"
+// 		});
+// });
--- script/login.js
+++ script/login.js
@@ -1,11 +1,28 @@
-//
-indexApp.controller('doLogin',function($scope){
-	$scope.isLogined = false;
+// 登录、注册、退出控制器
+indexApp.controller('doLogin',function($scope,$http){
+	$scope.isLogined = window.sessionStorage.isLogined == 'true' ? true : false;
+	$scope.userName = window.sessionStorage.curUser == 'undefined' ? undefined :  window.sessionStorage.curUser;
+	$scope.showLogin = function () {
+		var loginModal = document.getElementById('loginModal');
+		angular.element(loginModal).css('display','block');
+	};
 	$scope.login = function () {
-		$scope.userName = 'FantasyDL';
-		$scope.isLogined = true;
+		$http.get('login.json').success(function(result){
+			var loginModal = document.getElementById('loginModal');
+			angular.element(loginModal).css('display','none');
+			window.sessionStorage.curUser = result.nickname;
+			$scope.userName = result.nickname;
+			window.sessionStorage.isLogined = true;
+			$scope.isLogined = true;
+		});
 	};
 	$scope.register = function () {
-
+		
+	};
+	$scope.exit = function () {
+		window.sessionStorage.curUser = undefined;
+		$scope.userName = undefined;
+		window.sessionStorage.isLogined = false;
+		$scope.isLogined = false;
 	};
 });
