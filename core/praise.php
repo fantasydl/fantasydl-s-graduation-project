76a96283e9c26fac818816e40393d08c5f595084
fantasydl <406215730@qq.com>
2016-05-11 09:05:12 +0800

    update praise
    update praise

---
+++ core/praise.php
@@ -0,0 +1,28 @@
+<?php
+require_once '../include.php';
+
+$str = file_get_contents("php://input");
+
+$arr = json_decode($str, true);
+
+$commentid = $arr['commnetid'];
+
+$isCom = $arr['isCom'];
+
+$arr = array();
+
+$arr['praise'] = "praise+1";
+
+if($isCom){
+	if(update("comments", $arr, "commentid = '${commentid}'")){
+		echo "success";
+	}else{
+		echo "failed";
+	}
+}else{
+	if(update("tcomments", $arr, "commentid = '${commentid}'")){
+		echo "success";
+	}else{
+		echo "failed";
+	}
+}
